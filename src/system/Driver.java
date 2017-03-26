ef9cad2835e7e370f0c50a0b4bec5e4e023ab184
José Flôres <arokhyn@gmail.com>
2017-03-26 17:57:31 -0300

    Trocando para firefox.

--- src/system/Driver.java
+++ src/system/Driver.java
@@ -4,6 +4,8 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class Driver {
 	private static Driver instancia;
@@ -16,10 +18,15 @@ public class Driver {
 
 		if(OS.indexOf("win") >= 0){
 			System.setProperty("webdriver.chrome.driver", "libs\\chromedriver.exe");
+			driver = new ChromeDriver();
 		} else {
-			System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
+			//System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
+			System.setProperty("webdriver.gecko.driver", "/usr/local/bin/geckodriver");
+			DesiredCapabilities capabilities=DesiredCapabilities.firefox();
+			capabilities.setCapability("marionette", true);
+			driver = new FirefoxDriver(capabilities);
 		}
-		driver = new ChromeDriver();
+		
 	    baseUrl = "http://automationpractice.com/index.php";
 	    driver.get(baseUrl);
 	    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
